5385bc900503a0972566b78dcf43f17372f59fb8
Ken Suon <ken.su_dev@hotmail.com>
2021-10-19 23:13:50 +0200

    Work on UI Screens

---
+++ app/src/main/java/com/suonk/rickmortyapp/ui/fragments/SplashScreenFragment.kt
@@ -0,0 +1,22 @@
+package com.suonk.rickmortyapp.ui.fragments
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.suonk.rickmortyapp.R
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+}
---
+++ app/src/main/java/com/suonk/rickmortyapp/ui/fragments/main_pages/MainFragment.kt
@@ -0,0 +1,19 @@
+package com.suonk.rickmortyapp.ui.fragments.main_pages
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.suonk.rickmortyapp.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
